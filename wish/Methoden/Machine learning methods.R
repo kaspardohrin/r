b832eeb1d06c57909a60c4aedfbbb84812fef811
Tim <0949781@gmail.com>
2020-12-09 15:16:07 +0100

    edit file with methods

---
+++ wish/Methoden/Machine learning methods.R
@@ -0,0 +1,31 @@
+#machine learning methods
+
+https://lgatto.github.io/IntroMachineLearningWithR/an-introduction-to-machine-learning-with-r.html
+
+Packages:
+  caret
+ggplot2
+mlbench
+class
+caTools
+randomForest
+impute
+ranger
+kernlab
+class
+glmnet
+naivebayes
+rpart
+rpart.plot
+
+supervised learning, regression: Supervised learning problem that involves predicting a numerical label.
+
+https://machinelearningmastery.com/types-of-learning-in-machine-learning/
+  https://machinelearningmastery.com/support-vector-machines-for-machine-learning/
+  
+Patronen herkennen
+
+#k nearest neighbour= want hij cijfer word gebaseerd op fotos met de zelfde tags
+#(dit kan je zien als groups) hier gebruikt hij zijn ''neighbours'' om een cijfer te genereren
+#knn package
+
