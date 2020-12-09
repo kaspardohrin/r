ceb66ef322fe09428128a04c7fb1d37798007b1e
Kaspar Dohrin <kaspar_1994@live.nl>
2020-12-09 14:44:52 +0100

    remove unused code

--- wish/wish_classification.R
+++ wish/wish_classification.R
@@ -24,42 +24,13 @@ wish$mean_rating_unitssold <- (wish$rating_label + wish$units_sold_label) /2
 createRatingLabelColumn(wish)
 createQualityLabelColumn(wish)
 
-sub_selection <- head(wish)
-
-
 i = 0
 for(product in sub_selection$image_tags) {
   i <- i + 1;
   currentTaglist <- fromJSON(sub_selection$image_tags[i])
   sub_selection$image_tags_list[i] <- currentTaglist$result
-}
-
-i = 0
-for(product in sub_selection$image_tags) {
-  i <- i + 1;
   sub_selection$image_tags_list[[i]]$tag <- sub_selection$image_tags_list[[i]]$tag$en
 }
-
-# verplaats de tags een niveau hoger, i.e., $tags$en wordt $tags
-sub_selection$new_row <- list(c(list(sub_selection$image_tags_list[[1]]$tag, sub_selection$image_tags_list[[1]]$confidence)))
-
-
-#sub_selection$image_tags_list[[1]]$confidence
-#sub_selection$image_tags_list[[1]]$tag$en
-
-#length(sub_selection$image_tags_list[[1]]$confidence)
-
-
-#for(j in 1) { #:length(sub_selection$image_tags_list)
-
-  #collection <- list()
-  
-  #for (i in 1:length(sub_selection$image_tags_list[[j]][,1])) {
-    #collection <- append(collection, list(c(sub_selection$image_tags_list[[j]]$confidence[i], sub_selection$image_tags_list[[j]]$tag$en[i])))
-  #}
-  #print(collection)
-  #sub_selection$nieuwe_colom[j] <- df(collection)
-#}
 
 
 
